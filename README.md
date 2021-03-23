f5104e32be6f1c67a5fbb79bd0e0b13312eab7fc
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-03-23 09:26:32 -0300

    docu in special maps + readme

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ Then, this project can be configured through CMake. Since some projects rely on 
 ## Examples
 ### Special Maps
 
-This sample project is used for demonstrating some of the unusual special mappings available in NeoPZ. Relies on graphical output.
+This sample project is used for demonstrating some of the unusual special geometrical mappings available in NeoPZ. It also shows how even these curved elements can be geometrically refined. It relies on graphical output.
 
 ### F17 Directional Refinement
 
- This project used a F17 aircraft as a model to demonstrate the capabilities of automatic directional refinement in NeoPZ. These capabilities might be useful, for instance, in the case of boundary layer analysis, hence the chosen example. The program first builds a mesh modelling the *outside* of the aircraft, and the aircraft is part of the boundary of the created mesh. Then, through sucessive iterations, it models the boundary layer of the jet. Relies on graphical output.
+ This project used a F17 aircraft as a model to demonstrate the capabilities of automatic directional refinement in NeoPZ. These capabilities might be useful, for instance, in the case of boundary layer analysis, hence the chosen example. The program first builds a mesh modelling the *outside* of the aircraft, and the aircraft is part of the boundary of the created mesh. Then, through sucessive iterations, it models the boundary layer of the jet. It relies on graphical output.
--- SpecialMaps/specialmaps.cpp
+++ SpecialMaps/specialmaps.cpp
@@ -1,7 +1,6 @@
-#include "pzgmesh.h"
-#include "TPZVTKGeoMesh.h"
+#include "pzgmesh.h"//geometrical mesh
+#include "TPZVTKGeoMesh.h"//for printing the mesh in .vtk format
 //special maps
-#include "TPZCylinder.h"//@TODO: check if this class is still useful
 #include "TPZQuadTorus.h"
 #include "TPZTriangleTorus.h"
 #include "tpzellipse3d.h"
@@ -16,47 +15,88 @@
 #include "tpzquadraticprism.h"
 #include "tpzquadraticquad.h"
 #include "tpzquadratictrig.h"
-//#include "tpzblendnaca.h"//@TODO: create InsertExampleElement for this mapping
-#include "tpzgeoblend.h"
 
+
+/*
+This function will first create a mesh with just one element
+of the type TGeo. Then, this mesh is refined nref times
+for better visualization.
+*/
 template <class TGeo>
-void CreateSampleElement(const int &nref);
+void CreateSampleElement(const int nref);
 
 int main(int argc, char *argv[]) {
-    const int nRef = 4;
-    // CreateSampleElement<pzgeom::TPZCylinderMap<pzgeom::TPZGeoQuad>>(nRef);
+    constexpr int nRef = 4;
+
+    /*The first two classes create an element based on a torus
+      whose axis is aligned with the z-direction.*/
+    
+    //Creating a (incomplete) torus from a quadrilateral
     CreateSampleElement<pzgeom::TPZQuadTorus>(nRef);
+    //Creating a (incomplete) torus from a triangle.
     CreateSampleElement<pzgeom::TPZTriangleTorus>(nRef);
+
+    //Creating an element that maps a lign segment to an ellipse arc
     CreateSampleElement<pzgeom::TPZEllipse3D>(nRef);
-    CreateSampleElement<pzgeom::TPZQuadraticLine>(nRef);
-    CreateSampleElement<pzgeom::TPZQuadraticPyramid>(nRef);
-    CreateSampleElement<pzgeom::TPZQuadraticTetra>(nRef);
-    CreateSampleElement<pzgeom::TPZQuadSphere<pzgeom::TPZGeoQuad>>(nRef+3);
-    CreateSampleElement<pzgeom::TPZTriangleSphere<pzgeom::TPZGeoTriangle>>(nRef+3);
+
+    //Creating one half of a spherical shell from a quadrilateral
+    CreateSampleElement<pzgeom::TPZQuadSphere<pzgeom::TPZGeoQuad>>(nRef);
+    //Creating one quarter of a spherical shell from a triangle
+    CreateSampleElement<pzgeom::TPZTriangleSphere<pzgeom::TPZGeoTriangle>>(nRef);
+    //Mapping a line segment to a cosine
     CreateSampleElement<pzgeom::TPZWavyLine>(nRef);
+    //Mapping a line segment to an arc.
+    
+    /*This element is really useful interfaces in 2D problems
+      with curved geometries.*/
     CreateSampleElement<pzgeom::TPZArc3D>(nRef);
-    CreateSampleElement<pzgeom::TPZQuadraticCube>(nRef);
-    CreateSampleElement<pzgeom::TPZQuadraticPrism>(nRef);
-    CreateSampleElement<pzgeom::TPZQuadraticQuad>(nRef);
+
+    /*The following are elements obtained using through
+      quadratic lagrangian functions*/
+    CreateSampleElement<pzgeom::TPZQuadraticLine>(nRef);
     CreateSampleElement<pzgeom::TPZQuadraticTrig>(nRef);
+    CreateSampleElement<pzgeom::TPZQuadraticQuad>(nRef);
+    CreateSampleElement<pzgeom::TPZQuadraticTetra>(nRef);
+    CreateSampleElement<pzgeom::TPZQuadraticCube>(nRef);
+    CreateSampleElement<pzgeom::TPZQuadraticPyramid>(nRef);
+    CreateSampleElement<pzgeom::TPZQuadraticPrism>(nRef);
 }
 
 
 
 
 template <class TGeo>
-void CreateSampleElement(const int &nref){
+void CreateSampleElement(const int nref){
     std::string elName = TGeo::TypeName();
 
     std::cout<<"Starting mesh refinement of element type: "<<elName<<std::endl;
 
-    const int matId = 1;
+    //material identifier
+    constexpr int matId = 1;
+    /*
+      Geometric coordinates in NeoPZ are always assumed to be in a
+      three dimensional space.
+
+      The function of the parameters lowerCorner and size will differ
+      from element to element.
+
+      TPZManVector<TYPE,N> are vectors with bound checking and static
+      allocation (with dinamic allocation being possible if needed)*/
     TPZManVector<REAL,3> lowerCorner(3,0);
     TPZManVector<REAL,3> size(3,1);
+    //Geometrical mesh
     TPZGeoMesh gmesh;
     TGeo::InsertExampleElement(gmesh, matId, lowerCorner, size);
+    /*
+      Computes the neighbouring relationship in the mesh.
+      Necessary for the refinement process.
+     */
     gmesh.BuildConnectivity();
 
+    /*In the following loop, each element that has not been
+     divided (this check is performed through
+     TPZGeoEl::NSubElements()) will be refined using its default
+     refinement pattern.*/
     {
         TPZVec<TPZGeoEl *> sons;
         for(int iDiv = 0; iDiv < nref; iDiv++){
@@ -69,6 +109,8 @@ void CreateSampleElement(const int &nref){
             }
         }
     }
+
+    //outputs the mesh in .vtk format
     std::ofstream fileName(elName+".vtk");
     TPZVTKGeoMesh::PrintGMeshVTK(&gmesh, fileName);
 }
